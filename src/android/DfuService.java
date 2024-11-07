1723fc36d85e3cd3a274b1cf1cc0ceac89abf7dd
Dominik Wenger <info@2printbeta.de>
2024-11-07 19:17:38 +0100

    go back to dfu lib 1.12.0 and try to override the problematic function ?

--- src/android/DfuService.java
+++ src/android/DfuService.java
@@ -3,6 +3,26 @@ package de.einfachhans.DfuUpdate;
 import android.app.Activity;
 import no.nordicsemi.android.dfu.DfuBaseService;
 
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Build;
+import org.jetbrains.annotations.Nullable;
+
+
+
+@Override
+public Intent registerReceiver(@Nullable BroadcastReceiver receiver, IntentFilter filter) {
+
+    if (Build.VERSION.SDK_INT >= 34 && getApplicationInfo().targetSdkVersion >= 34) {
+      return super.registerReceiver(receiver, filter, Context.RECEIVER_EXPORTED);
+    } else {
+      return super.registerReceiver(receiver, filter);
+    }
+
+}
+
+
 public class DfuService extends DfuBaseService {
 
     @Override
